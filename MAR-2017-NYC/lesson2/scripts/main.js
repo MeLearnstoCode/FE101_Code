fb7533fea1e58dbecbed42449f3a8eb37b3a1ae6
Nicole Stein <nicole.stein@nycda.com>
2017-04-06 21:33:43 -0400

    lesson9

--- MAR-2017-NYC/lesson2/scripts/main.js
+++ MAR-2017-NYC/lesson2/scripts/main.js
@@ -6,13 +6,13 @@ $('.menu-icon').click(function() {
 	$('nav li').slideToggle();
 });
 
-var menu = $('nav li');
+var $menu = $('nav li');
 
 // Show nav again when page is resized.
 $(window).resize(function() {
 	var width = $(window).width();
 
-	if (width > 768 && menu.is(':hidden')) {
-		menu.removeAttr('style');
+	if (width > 768 && $menu.is(':hidden')) {
+		$menu.removeAttr('style');
 	}
 });
