a82f8d888642aa4a0946be0deec63694077afa9b
Nicole Stein <nicole.stein@nycda.com>
2017-06-15 21:39:02 -0400

    lesson 9

---
+++ MAY-2017-NYC/lesson9.js
@@ -0,0 +1,13 @@
+const isRaining = true;
+if (isRaining) {
+	console.log('bring an umbrella');
+} else {
+	console.log('enjoy a sunny day');
+}
+
+const x = 5;
+if (x > 0) {
+	console.log('x is positive');
+} else {
+	console.log('x is not positive');
+}
