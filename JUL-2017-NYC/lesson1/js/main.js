bae0ab007dd2d66f89700f77385b1498e6ff12ab
Nicole Stein <nicolethenerd@gmail.com>
2017-07-24 21:35:01 -0400

    lesson1 templateSASS

--- JUL-2017-NYC/lesson1/js/main.js
+++ JUL-2017-NYC/lesson1/js/main.js
@@ -1,6 +1,21 @@
+var nav = $('nav ul');
+
 $('nav').onePageNav();
 
 // When #menu-icon is clicked
 $('#menu-icon').click(function() {
-	$('nav').slideToggle();
+	nav.slideToggle();
+});
+
+// When I resize the window
+$(window).resize(function() {
+   	// Get the width
+    var width = $(window).width();
+
+    // If width is bigger than breakpoint
+    // and nav is hidden
+	if(width > 768 && nav.is(':hidden')) {
+		// Remove the 'style' attribute from the nav
+	    nav.removeAttr('style');
+	}
 });
