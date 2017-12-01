4f80836d8e182c73bd44d26bf16f13e28de97194
Nicole Stein <nicolethenerd@gmail.com>
2017-11-30 21:32:59 -0500

    lessons 2 and 3

--- NOV-2017-NYC/lesson1/js/main.js
+++ NOV-2017-NYC/lesson1/js/main.js
@@ -3,3 +3,16 @@ $('nav').onePageNav();
 $('#nav-btn').click(function() {
 	$('nav').slideToggle();
 });
+
+var nav = $('nav');
+
+// When user resizes the window
+$(window).resize(function() {
+	// Get the width of the page
+   	var width = $(window).width();
+   	// If width > breakpoint and the nav is hidden
+   	if (width > 768 && nav.is(':hidden')) {
+   		// Remove the style attribute
+      nav.css('display', '');
+   	}
+});
