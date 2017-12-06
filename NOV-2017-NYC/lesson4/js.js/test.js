d6a8561cfd0a30f9e349ca2e592587ac38751c8a
Nicole Stein <nicolethenerd@gmail.com>
2017-12-05 21:34:53 -0500

    lesson4

---
+++ NOV-2017-NYC/lesson4/js.js/test.js
@@ -0,0 +1,32 @@
+console.log('Hello world!');
+
+// var x = 5; // NO!
+// x = 6;
+
+// Constants - do not override
+const x = 5;
+// x = 6; // NO!
+
+let y = 7;
+y = 10;
+
+console.log('hi');
+console.log('foo');
+
+const greeting = 'Hello'; // string
+const isTuesday = true; // boolean
+// const numStudents = 6; // number
+
+// let numStudents; // undefined
+
+numStudents = 6;
+
+function sayHi() {
+  prefix = 'Hi ';
+}
+
+const person = {
+  firstName: 'Nicole',
+  lastName: 'Stein',
+  age: null,
+};
