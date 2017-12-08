0419619557c765550e5562fa19982297ff1a5fb0
Nicole Stein <nicolethenerd@gmail.com>
2017-12-07 21:42:44 -0500

    lesson 5

---
+++ NOV-2017-NYC/lesson5.js
@@ -0,0 +1,19 @@
+const numChildren = 7;
+const partnerName = 'Joel';
+const geoLocation = 'Fiji';
+const jobTitle = 'astronaut';
+
+console.log('You will be a ' + jobTitle + ' in ' + geoLocation + ', and married to ' + partnerName + ' with ' + numChildren + ' kids.');
+
+console.log(`You will be a ${jobTitle} in ${geoLocation}, and married to ${partnerName} with ${numChildren} kids.`);
+
+const birthYear = 2000;
+const futureYear = 2050;
+
+const age = futureYear - birthYear;
+console.log(`I will be either ${age - 1} or ${age} in ${futureYear}.`)
+console.log('I will be either ' + (age - 1) + ' or ' + age + ' in ' + futureYear + '.');
+
+const userName = prompt('What is your name?');
+alert(`Hello ${userName}!`);
+
