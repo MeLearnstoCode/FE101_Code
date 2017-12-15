3abbc1f981ec3d628da2c123f05977871883636a
Nicole Stein <nicolethenerd@gmail.com>
2017-12-14 21:43:50 -0500

    lesson7

---
+++ NOV-2017-NYC/lesson7/undefined_example.js
@@ -0,0 +1,16 @@
+const homework = null;
+let numStudents;
+
+if (homework) {
+  console.log('Do your homework');
+} else {
+  console.log('There is no homework');
+}
+
+if (numStudents) {
+  console.log(`There are ${numStudents} students.`);
+} else {
+  console.log('There are no students here yet.');
+}
+
+console.log(x);
